3282d2dbaf667f698185515b4a04861399fb8599
andrewdrive <blank@mail.com>
2022-05-26 18:00:58 +0300

    v3

--- project/api/models.py
+++ project/api/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 
-#class 
+class Message(models.Model):
+    class Status(models.TextChoices):
+        REW = '1', "review"
+        BLO = '2', "blocked"
+        COR = '3', "correct"
 
+    text = models.CharField(max_length=1024, default='sample text', blank=True, verbose_name='текст сообщения')
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.REW, blank=True, verbose_name='статус сообщения')
+
+
+    class Meta:
+        ordering = ['status']
+    
+    def __str__(self):
+        return self.text
 
 
 # Create your models here.
