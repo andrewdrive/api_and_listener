5ecb03b8031967f270369a474bbd99eb0e85f792
andrewdrive <blank@mail.com>
2022-05-27 16:56:50 +0300

    ...

--- project/api/views.py
+++ project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, mixins
 from rest_framework import permissions
+from rest_framework.response import Response
 from api.models import  Message
 from api.serializers import  MessageSerializer
 
@@ -11,6 +12,11 @@ class MessageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
+    def create(self, request, *args, **kwargs):
+        response = {}
+        print('HELLO')
+        return Response(response)
+        
 
 
 # Create your views here.
